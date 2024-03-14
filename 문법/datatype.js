95683da88489ad44bbc04943ea7277be8ec370d0
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2024-03-14 15:36:28 +0900

    기초

---
+++ 문법/datatype.js
@@ -0,0 +1,7 @@
+// js에서 false로 처리되는 값들
+// null,Nan,0,빈 문자열, undefined,false
+
+if(undefined)
+console.log("참")
+else
+console.log("거짓")
---
+++ 문법/exam.js
@@ -0,0 +1,2 @@
+console.log('hello world');
+console.log("제발좀 닥쳐 너무 시끄러워 왔다갔다 하ㅣ도 말고 제발좀 닥쳐");
