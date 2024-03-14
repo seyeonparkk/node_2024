223283cb091c64a004b69242631471acfe016a46
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2024-03-14 15:42:28 +0900

    let var 차이

---
+++ 문법/let.js
@@ -0,0 +1,17 @@
+function varTest(){
+    var v=1;
+    {
+        var v=2;
+        console.log(v);
+    }
+    console.log(v)
+}
+
+function varTest(){
+    let v=1;
+    {
+        let v=2;
+        console.log(v);
+    }
+    console.log(v)
+}
