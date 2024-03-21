24fc141528e97da17428bfe9ecdebdd54a9eec39
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2024-03-21 15:26:42 +0900

    동등

---
+++ 문법/6)callback.js
@@ -0,0 +1,1 @@
+setTimeout(()=>console.log("5초 경과"),5000);
---
+++ 문법/7)equality.js
@@ -0,0 +1,11 @@
+//all TRUE
+console.log(1==1);
+console.log(1=='1');
+console.log(0==false);
+console.log(0=='');
+
+
+console.log(1===1);   //true
+console.log(1==='1');  //false
+console.log(0===false);  //false
+console.log(0==='');    //false
