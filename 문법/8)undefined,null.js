4b7ea180b08a50b2ce969c82b72236e494415b54
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2024-03-21 15:41:39 +0900

    null과 undefined의 비교

---
+++ 문법/8)undefined,null.js
@@ -0,0 +1,12 @@
+let foo;
+console.log(foo);
+
+const obj={}
+console.log(obj.아무속성);
+
+let bar=null
+console.log(null);
+
+//주의할 것
+console.log(null==undefined)  //true
+console.log(null===undefined)  //false
